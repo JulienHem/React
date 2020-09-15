b047c04ccfb565bdc4ffe86902a6e8b2619e3002
JulienHem <julien.hemmery@gmail.co>
2020-09-15 16:52:16 +0200

    Fin de l'app !

---
+++ build/precache-manifest.90099c1390efca38c1e7fa912be7a8af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a210044a032d50dff4853df198da73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b9eff2c3f44ffc4b29",
+    "url": "/static/css/main.9462a9f0.chunk.css"
+  },
+  {
+    "revision": "b94270e179c8f5a3463f",
+    "url": "/static/js/2.66ca3648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66ca3648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b9eff2c3f44ffc4b29",
+    "url": "/static/js/main.d40ddb1a.chunk.js"
+  },
+  {
+    "revision": "f9fb49613a98649d81d2",
+    "url": "/static/js/runtime-main.41be737a.js"
+  }
+]);
